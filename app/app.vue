a4e3004d197419738da6cb02475243384edead3a
byteslash <serg.jn@gmail.com>
2025-11-09 09:56:25 +0200

    feat: configure sonner

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,10 @@
+<script setup lang="ts">
+import { Toaster } from "@/components/ui/sonner";
+import "vue-sonner/style.css"; // vue-sonner v2 requires this import
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLayout />
+  <Toaster rich-colors position="top-right" :close-button="true" close-button-position="top-left" />
 </template>
